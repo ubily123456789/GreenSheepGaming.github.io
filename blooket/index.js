1b89b73909558efb6ac3e28ea0fce9d460e53fb3
GreenSheepGaming <103353833+GreenSheepGaming@users.noreply.github.com>
2023-01-11 23:34:16 +0000

    dev

--- blooket/index.js
+++ blooket/index.js
@@ -1,7 +1,142 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-const a = document.getElementById("desc")
+var mainpage = document.getElementById("maintext");
+var Goldhid = document.getElementsByClassName("Goldhid");
+mainpage.style = "Padding: 5%; Padding-Left: 10%;"
 
-var css = '.desc {animation-name:test; animation-duration:4s; animation-iteration-count:infinite; } @keyframes test{ 0%{color:#ff0000} 20%{color:#00ff00} 40%{color:#ffff00} 60%{color:#0000ff} 80%{color:#00ffff} 100%{color:#ff0000}', head = document.head || document.getElementsByTagName('head')[0], style = document.createElement('style'); style.type = 'text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style);
-var css = '.listing {animation-name:test; animation-duration:4s; animation-iteration-count:infinite; } @keyframes test{ 0%{color:#ff0000} 20%{color:#00ff00} 40%{color:#ffff00} 60%{color:#0000ff} 80%{color:#00ffff} 100%{color:#ff0000}', head = document.head || document.getElementsByTagName('head')[0], style = document.createElement('style'); style.type = 'text/css'; if (style.styleSheet){ style.styleSheet.cssText = css; } else { style.appendChild(document.createTextNode(css)); } head.appendChild(style);
+
+function Crypto(){
+    mainpage.innerHTML = "javascript:(async () => {let n = document.createElement('iframe');document.body.append(n);window.alert = n.contentWindow.alert.bind(window);window.prompt = n.contentWindow.prompt.bind(window);window.confirm = n.contentWindow.confirm.bind(window);n.remove();function reactHandler() {return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;};if (window.location.pathname != '/play/hack') {alert('You must be in a crypto hack game mode to use this hack!');} else {let amount = Number(parseFloat(prompt('How much crypto do you want?')));reactHandler().stateNode.setState({ crypto2: amount, crypto: amount });alert('Crypto added!');};})();"
+}
+
+function Showgold(){
+    var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function Gold1(){
+    mainpage.innerHTML = "javascript:(async () => {let n = document.createElement('iframe');document.body.append(n);window.alert = n.contentWindow.alert.bind(window);window.prompt = n.contentWindow.prompt.bind(window);window.confirm = n.contentWindow.confirm.bind(window);n.remove();if (window.location.pathname != '/play/gold') {    alert('You must be in a gold quest game!');} else {   alert('Chest ESP enabled');interval = setInterval(() => {if (window.location.pathname != '/play/gold') return clearInterval(interdata = Object.values(document.quelector('#app > div > div'))[1].children[1]._owner.stateNode.sif (data?.stage == 'prize') {boxes = data.choices;choiceDiv = document.querySelector(\"div[class*='arts__regularBody\']\")?.children[1];if (!choiceDiv) return clearInterval(interval)if (!document.querySelector(\"p[class*=\'chest-esp\']\")) boxes.forEach((box, i) => {textElement = document.createElement(\'p\');textElement.className = \"chest-esp\";textElement.innerText = box.text;textElement.style = `text-align: center;font-size: 30px;color: white;font-family:Titan One;sans-serif;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;border-color: black;margin-top: 200px;`try { choiceDiv.children[i].appendChild(textElement); } catch (e) {if (confirm(\'An error occured, would you like to report this in the support discord server?\')) {window.open(\'https://glizzers.xyz/discord\');};};});else boxes.forEach((box, i) => {if (choiceDiv.children.length == 3 && choiceDiv.children[i].children[1].innerText != box.text) choiceDiv.children[i].children[1].innerText = box.text;});};if (window.location.pathname != \'/play/gold\') clearInterval(interval)}, 0);};})();";
+}
+function Gold2(){
+    mainpage.innerHTML = "javascript:(async () => {let n = document.createElement('iframe');document.body.append(n);window.alert = n.contentWindow.alert.bind(window);window.prompt = n.contentWindow.prompt.bind(window);window.confirm = n.contentWindow.confirm.bind(window);n.remove();function reactHandler() {return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;};if (window.location.pathname != '/play/gold') {alert('You must be in a gold quest game!');} else {let gold = Number(parseFloat(prompt('How much gold do you want?')));reactHandler().stateNode.setState({ gold2: gold, gold });alert('Gold added!');};})();";
+}
+function Gold3(){
+    mainpage.innerHTML = "javascript:(() => {let n = document.createElement('iframe');document.body.append(n);window.alert = n.contentWindow.alert.bind(window);window.prompt = n.contentWindow.prompt.bind(window);window.confirm = n.contentWindow.confirm.bind(window);n.remove();function reactHandler() {return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner;};if (window.location.pathname != '/play/gold') {alert('You must be in a gold quest game!');} else {let e = reactHandler(),player = prompt(\"Player name to set gold\"),amount = Number(parseFloat(prompt(\"Amount to set gold to\")));e.memoizedProps.firebase.setVal({id: e.memoizedProps.client.hostId,path: \"c/\" + e.memoizedProps.client.name,val: {b: e.memoizedProps.client.blook,g: e.stateNode.state.gold,tat: player + \":swap:\" + amount}})alert('Set gold for player:' + player + ' to ' + amount);};})();"
+}
+function Showtower1(){
+    var d = document.getElementById("Towerhid1");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+function tower11(){
+    // clear enimes
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/defense') { alert('You must be in a tower defense game!'); } else { reactHandler().stateNode.enemies = []; alert('Enemies cleared!'); }; })();"
+}
+function tower12(){
+    // max out tower
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/defense') { alert('You must be in a Tower Defense game!'); } else { reactHandler().stateNode.towers.forEach(tower => { tower.damage = 1e308; tower.range = 1e308; tower.level = 3; tower.blastRadius = 1e308 tower.fullCd = 0; }); alert('Maxed out towers!'); }; })();"
+}
+function tower13(){
+    // set cash
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/defense') { alert('This cheat only supports tower defence game mode!') } else { let tokens = Number(parseFloat(prompt('How many tokens do you want?'))); reactHandler().stateNode.setState({ tokens }); alert('Tokens set!'); }; })();"
+}
+function tower1(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname == '/tower/battle' || window.location.pathname == '/tower/start') { try { reactHandler().stateNode.props.setTowerCoins(Number(parseFloat(prompt('How many coins do you want?')))); alert('Coins added!') } catch { } } else { alert('You must be in a tower of doom game!'); }; })();"
+}
+function tower2(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname == '/tower/battle' || window.location.pathname == '/tower/start') { let data = reactHandler().stateNode.state; if (data.phase != 'select') return alert('You must use this hack while you are starting a match or in one!'); reactHandler().stateNode.setState({ myCard: { ...data.myCard, strength: 100, charisma: 100, wisdom: 100 } }) alert('Done!') } else { alert('You must be in a Tower of Doom game!'); }; })();"
+}
+function tower3(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname == '/tower/battle' || window.location.pathname == '/tower/start') { let data = reactHandler().stateNode.state; if (data.phase != 'select') return alert('You must be on the attribute selection page!'); reactHandler().stateNode.setState({ enemyCard: { ...data.enemyCard, strength: 0, charisma: 0, wisdom: 0 } }) alert('Enemy stats lowered!') } else { alert('You must be in a tower of doom game!'); }; })();"
+}
+
+function Showtower(){
+    var d = document.getElementById("towerhid");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+function Showtower4(){
+    var d = document.getElementById("towerhid4");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+
+function tower14(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/play/racing') { alert('You must be in a racing game!'); } else { reactHandler().stateNode.setState({ progress: reactHandler().stateNode.state.goalAmount }); setTimeout(() => { try { Array.from(document.body.querySelectorAll('div[class*=\"answerText\"]')).filter(t => t.firstChild.innerHTML == reactHandler().memoizedState.question.correctAnswers[0])[0].click(); } catch { try { Array.from(document.body.querySelectorAll('div[class*=\"answerText\"]')).filter(t => t.firstChild.innerHTML == reactHandler().memoizedProps.client.question.correctAnswers[0])[0].click(); } catch { }; }; }, 100); }; })();"
+}
+function Showtower9(){
+    var d = document.getElementById("towerhid9");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+
+function tower19(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/play/rush') { alert('You must be in a blook rush game mode to use this hack!'); } else { let e = reactHandler(); e.stateNode.props.firebase.setVal({ id: e.stateNode.props.client.hostId, path: 'c/' + e.stateNode.props.client.name + '/bs', val: Number(parseFloat(prompt('How many blooks do you want?'))) }); alert('Blooks set!'); }; })();"
+}
+function tower29(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/play/rush') { alert('You must be in a blook rush game mode to use this hack!'); } else { let e = reactHandler(); e.stateNode.props.firebase.setVal({ id: e.stateNode.props.client.hostId, path: 'c/' + e.stateNode.props.client.name + '/d', val: Number(parseFloat(prompt('How much defense do you want?'))) }); alert('Defense set!'); }; })();"
+}
+
+function Showtower7(){
+    var d = document.getElementById("towerhid7");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+function tower17(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (document.location.pathname != \"/cafe\") { alert(\"You must be in a cafe game!\"); } else { reactHandler().stateNode.state.foods.forEach(e => e.stock = 99999); reactHandler().stateNode.forceUpdate(); alert('Added infinite food level!'); } })();"
+}
+function tower27(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (document.location.pathname != \"/cafe\") { alert(\"You must be in a cafe game!\"); } else { reactHandler().stateNode.setState({ cafeCash: Number(parseFloat(prompt('How much cash would you like?'))) }); alert('Added cash!'); }; })();"
+}
+function Showtower5(){
+    var d = document.getElementById("towerhid5");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+function tower15(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/kingdom') { alert('You must be in a crazy kingdom game!'); } else { reactHandler().stateNode.setState({ materials: 100, people: 100, happiness: 100, gold: 100 }); alert('max stats set!'); }; })();"
+}
+function tower25(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/kingdom') { alert('You must be in a crazy kingdom game!'); } else { let guestScore = Number(parseFloat(prompt('How many guests do you want?'))); reactHandler().stateNode.setState({ guestScore }); alert('Guests set!'); }; })();"
+}
+function tower35(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/kingdom') { alert('You must be in a crazy kingdom game!'); } else { reactHandler().stateNode.nextGuest(); alert('Guest skipped!'); }; })();"
+}
+function Showtower6(){
+    var d = document.getElementById("towerhid6");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+}
+}
+function Showtower8(){
+    var d = document.getElementById("towerhid8");
+  if (d.style.display === "none") {
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+}
+function tower18(){
+    mainpage.innerHTML = "javascript:(async () => { let n = document.createElement('iframe'); document.body.append(n); window.alert = n.contentWindow.alert.bind(window); window.prompt = n.contentWindow.prompt.bind(window); window.confirm = n.contentWindow.confirm.bind(window); n.remove(); function reactHandler() { return Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner; }; if (window.location.pathname != '/play/fishing') { alert('You must be in a fishing frenzy game!'); } else { let weight = Number(parseFloat(prompt('How much weight do you want?'))); reactHandler().stateNode.setState({ weight2: weight, weight }); alert('Weight set!'); }; })();";
+}
