e3141cf4fe908c78c5b72291de96c071ab0aade4
GreenSheepGaming <103353833+GreenSheepGaming@users.noreply.github.com>
2023-02-23 07:04:54 +0000

    deev

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 window.onload = function() {
     // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyD1w6li8sNNplMAEE2fnnb2a5ktr9ZZ8Rw",
-        authDomain: "tptdts.firebaseapp.com",
-        projectId: "tptdts",
-        storageBucket: "tptdts.appspot.com",
-        messagingSenderId: "541065404521",
-        appId: "1:541065404521:web:f814719019f969451182f8",
-        measurementId: "G-03ED7ZWTSN"
+      apiKey: "AIzaSyD1w6li8sNNplMAEE2fnnb2a5ktr9ZZ8Rw",
+      authDomain: "tptdts.firebaseapp.com",
+      databaseURL: "https://tptdts-default-rtdb.firebaseio.com",
+      projectId: "tptdts",
+      storageBucket: "tptdts.appspot.com",
+      messagingSenderId: "541065404521",
+      appId: "1:541065404521:web:f814719019f969451182f8",
+      measurementId: "G-03ED7ZWTSN"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
